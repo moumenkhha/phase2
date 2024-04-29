d29205e500737c74c80e7b7475533f8d722b9c76
Moumen <ma1910156@qu.edu.qa>
2024-04-29 14:40:37 +0300

    Saving

--- 00-relational-mapping/app/page.jsx
+++ 00-relational-mapping/app/page.jsx
@@ -2,9 +2,10 @@ import * as users from "@/repository/users";
 export default async function Home() {
   const result = await users.get();
   return result.map(user =>
-    <div key={user.username}>
+    <div className="flex gap-x-2" key={user.username}>
       <h1>Name: {user.name} {user.surname}</h1>
-      <p>Balance: {user.balance}</p>
+      {/* <p>Balance: {user.balance}</p> */}
+      <button className="text-red-500" onClick={(e) => console.log("h")}>Delete</button>
     </div>
   );
 }
